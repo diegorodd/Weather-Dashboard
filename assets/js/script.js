db1ce9eb838ea1722c9205e99bccbcae1aafec4e
diegorodd <dieegoa@icloud.com>
2022-06-12 21:51:08 -0400

    added comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,10 @@
 function Info(){
+    // new name is for the user to put whichever city they want
     const newName = document.getElementById("cityInput");
     const cityName =document.getElementById("cityName");
     cityName.innerHTML = "--"+newName.value+"--"
 
-
+    // make the fetch get any city its searched for
 fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=03e4117d4ffcfe106f7d287a2cdf04a1")
 .then(Response => Response.json())
 .then(data =>{
